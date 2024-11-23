bbb3e19f46156af46abd245f4bff8a5a691704f1
dcblakeman <dcblakeman@gmail.com>
2024-11-23 15:47:21 -0500

    Changed Login Form name to LoginForm.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Scheduling_Desktop_UI_App
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
